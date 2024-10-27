99bd02d74b6201ef2683c61a8d23822950c4c826
Nurislom <nurislomx72@gmail.com>
2024-10-27 18:43:19 +0500

    Добавлены новые классы для ErrorHttpStatus. #10-27

--- src/main/java/org/khasanof/core/error/BasicError.java
+++ src/main/java/org/khasanof/core/error/BasicError.java
@@ -1,9 +1,20 @@
 package org.khasanof.core.error;
 
+import org.springframework.http.HttpStatus;
+
 /**
  * @author Nurislom
  * @see org.khasanof.core
  * @since 9/5/2024 6:32 PM
  */
-public interface BasicError extends ErrorKey, ErrorMessage, ErrorEntityName {
+public interface BasicError extends ErrorKey, ErrorMessage, ErrorEntityName, ErrorHttpStatus {
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    default HttpStatus getHttpStatus() {
+        return HttpStatus.BAD_REQUEST;
+    }
 }
