ed95a1e8937a7759fe3f1b6e8b37c0f6cbfea699
Nurislom <nurislomx72@gmail.com>
2025-03-15 12:37:16 +0500

    started hotel reservation system. #03-15

--- src/main/java/org/khasanof/core/domain/types/IIdentified.java
+++ src/main/java/org/khasanof/core/domain/types/IIdentified.java
@@ -1,11 +1,13 @@
 package org.khasanof.core.domain.types;
 
+import java.io.Serializable;
+
 /**
  * @author Nurislom
  * @see org.khasanof.core.domain.types
  * @since 12/4/2024 5:25 PM
  */
-public interface IIdentified {
+public interface IIdentified extends Serializable {
 
     /**
      *
